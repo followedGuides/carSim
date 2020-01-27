d0f6feb0b6f00a74e4f9a600fdcdd1d8c539b327
zyprezz@gmail.com <zyprezz@gmail.com>
2020-01-27 13:03:31 +0100

    first commit

---
+++ carSim/ClientApp/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Dash from './pages/Dash'
+
+import './custom.css'
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+        <Dash />
+    );
+  }
+}
---
+++ carSim/ClientApp/src/components/pages/Dash.js
@@ -0,0 +1,11 @@
+﻿import React from 'react'
+
+
+export default class Dash extends React.Component {
+
+
+    return(
+        <div>
+
+    )
+}
